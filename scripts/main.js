4ac60c280a2077c1e02160e478493a1b978b6d58
jgagarin <jon.gagarin@gmail.com>
2015-01-27 21:32:38 -0800

    added scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     $(".toggler").on('click', function(){
-        $(".main-nav").slideToggle();
+        $(".main-nav").slideToggle('slow');
+    });
+    $(".main-nav").on('click',function(){
+        $(this).slideToggle('slow');
     });
 });
