8963a4a9d07bdbe3dd8d14820a1f931699763ea0
jgagarin <jon.gagarin@gmail.com>
2015-02-04 09:34:07 -0800

    smooth scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
+    
+    //disappear toggle nav
     $(".toggler").on('click', function(){
         $(".main-nav").slideToggle('slow');
     });
     $(".main-nav").on('click',function(){
         $(this).slideToggle('slow');
     });
+    
+    //smooth scroller
+    $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  }); //end smooth scroll
+    
 });
